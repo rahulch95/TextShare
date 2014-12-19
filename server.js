bdfb3bd83383851d295f1d1ea3159b0b5852d13b
Rahul Chaudhary <rahul.chaudhary@mail.utoronto.ca>
2014-12-18 22:59:25 -0500

    update 3

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
-var http = require("http");
+var express = require('express'),
+    app = express(),
+    request_lib = require('request'),
+    cheerio = require('cheerio');
+app.set('port', (process.env.PORT || 5000));
+// var server = http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/html"});
+//   response.write("<!DOCTYPE \"html\">");
+//   response.write("<html>");
+//   response.write("<head>");
+//   response.write("<title>Hello World Page</title>");
+//   response.write("</head>");
+//   response.write("<body>");
+//   response.write("Hello World! 5000");
+//   response.write("</body>");
+//   response.write("</html>");
+//   response.end();
+// });
+// var sendComments = function(response) {
+//   response.writeHead(200, {"Content-Type": "text/html"});
+//   response.write("<!DOCTYPE \"html\">");
+//   response.write("<html>");
+//   response.write("<head>");
+//   response.write("<title>Hello World Page</title>");
+//   response.write("</head>");
+//   response.write("<body>");
+//   response.write("Hello World! 5000");
+//   response.write("</body>");
+//   response.write("</html>");
+//   response.end();
+// });
 
-var server = http.createServer(function(request, response) {
+app.get('/', function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("<!DOCTYPE \"html\">");
   response.write("<html>");
@@ -13,6 +43,7 @@ var server = http.createServer(function(request, response) {
   response.write("</html>");
   response.end();
 });
- 
-server.listen(8080);
-console.log("Server is listening");
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
