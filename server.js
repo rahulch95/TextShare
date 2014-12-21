06bf71b5e74f25654cb64a88358beb45ec25e4bc
Rahul Chaudhary <rahul.chaudhary@mail.utoronto.ca>
2014-12-21 01:19:07 -0500

    added Jade and socket.io

--- server.js
+++ server.js
@@ -1,49 +1,38 @@
 var express = require('express'),
     app = express(),
+    http = require('http'),
+    server = http.createServer(app),
     request_lib = require('request'),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
 app.set('port', (process.env.PORT || 5000));
-// var server = http.createServer(function(request, response) {
-//   response.writeHead(200, {"Content-Type": "text/html"});
-//   response.write("<!DOCTYPE \"html\">");
-//   response.write("<html>");
-//   response.write("<head>");
-//   response.write("<title>Hello World Page</title>");
-//   response.write("</head>");
-//   response.write("<body>");
-//   response.write("Hello World! 5000");
-//   response.write("</body>");
-//   response.write("</html>");
-//   response.end();
-// });
-// var sendComments = function(response) {
-//   response.writeHead(200, {"Content-Type": "text/html"});
-//   response.write("<!DOCTYPE \"html\">");
-//   response.write("<html>");
-//   response.write("<head>");
-//   response.write("<title>Hello World Page</title>");
-//   response.write("</head>");
-//   response.write("<body>");
-//   response.write("Hello World! 5000");
-//   response.write("</body>");
-//   response.write("</html>");
-//   response.end();
-// });
 
 app.get('/', function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE \"html\">");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World! 5000");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
+  response.sendFile(__dirname + '/html/textpad.html');
+
 });
 
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'));
+
+server.listen(app.get('port'), function() {
+    console.log("Node app is running at port: " + app.get('port'));
 });
+
+  // response.writeHead(200, {"Content-Type": "text/html"});
+  // response.write("<!DOCTYPE \"html\">");
+  // response.write("<html>");
+  // response.write("<head>");
+  // response.write("<title>Hello World Page</title>");
+  // response.write("</head>");
+  // response.write("<body>");
+  // response.write("Hello World! 5000");
+  // response.write("</body>");
+  // response.write("</html>");
+  // response.end();
